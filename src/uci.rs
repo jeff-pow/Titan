58220e52cbe6c588f43c233d828727ab633cbbaa
Jeff Powell <jrpowel9@ncsu.edu>
2023-03-02 17:12:32 -0500

    reformatted whitespace to make more readable

--- src/uci.rs
+++ src/uci.rs
@@ -24,50 +24,76 @@ pub fn main_loop() -> ! {
     loop {
         buffer.clear();
         io::stdin().read_line(&mut buffer).unwrap();
+
         writeln!(file, "UCI said: {}", buffer).expect("File not written to");
+
         if buffer.starts_with("isready") {
             println!("readyok");
             writeln!(file, "readyok").expect("File not written to");
+
         } else if buffer.starts_with("debug on") {
             debug = true;
             println!("info string debug on");
+
         } else if buffer.starts_with("ucinewgame") {
             board = build_board(fen::STARTING_FEN);
+
         } else if buffer.starts_with("position") {
             let vec: Vec<&str> = buffer.split_whitespace().collect();
+
             if buffer.contains("fen") {
                 board = build_board(&parse_fen_from_buffer(&vec));
-                println!("info string {}", board);
+
+                if debug {
+                    println!("info string {}", board);
+                }
                 if vec.len() > 9 {
                     parse_moves(&vec, &mut board, 9, debug);
                 }
+
             } else if buffer.contains("startpos") {
                 board = build_board(fen::STARTING_FEN);
-                println!("info string\n {}", board);
+
+                if debug {
+                    println!("info string\n {}", board);
+                }
+
                 if vec.len() > 3 {
                     parse_moves(&vec, &mut board, 3, debug);
                 }
-                println!("info string\n {}", board);
+
+                if debug {
+                    println!("info string\n {}", board);
+                }
             }
+
         } else if buffer.starts_with("go") {
             let moves = generate_all_moves(&board);
             let m = moves.choose(&mut rand::thread_rng()).unwrap();
             println!("bestmove {}", m.to_lan());
             board.make_move(m);
-            println!("info string MOVE CHOSEN: {}\n {}", m, board);
+
+            if debug {
+                println!("info string MOVE CHOSEN: {}\n {}", m, board);
+            }
+
             writeln!(file, "{}", m.to_lan()).unwrap();
+
         } else if buffer.starts_with("stop") {
             std::process::exit(0);
+
         } else if buffer.starts_with("uci") {
             println!("id name Jeff's Chess Engine");
-            writeln!(file, "id name Jeff's Chess Engine").expect("File not written to");
             println!("id author Jeff Powell");
-            writeln!(file, "id author Jeff Powell").expect("File not written to");
             println!("uciok");
+
+            writeln!(file, "id name Jeff's Chess Engine").expect("File not written to");
+            writeln!(file, "id author Jeff Powell").expect("File not written to");
             writeln!(file, "uciok").expect("File not written to");
+
         } else {
             writeln!(file, "{}", buffer).unwrap();
-            panic!("Command not handled");
+            panic!("Command not handled: {}", buffer);
         }
     }
 }
@@ -76,9 +102,11 @@ fn parse_moves(moves: &[&str], board: &mut Board, skip: usize, debug: bool) {
     for str in moves.iter().skip(skip) {
         let m = from_lan(str, board);
         board.make_move(&m);
-        print!("info string making move {}\n {}", m, board);
-        println!("{}", m);
-        println!("{}", board);
-        println!("------------------------");
+        if debug {
+            print!("info string making move {}\n {}", m, board);
+            println!("{}", m);
+            println!("{}", board);
+            println!("------------------------");
+        }
     }
 }
