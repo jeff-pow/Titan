d6a12c75164db10179c1c1bbdf736a9c70841426
Jeff Powell <jrpowel9@ncsu.edu>
2023-03-07 20:24:30 -0500

    wrote a couple tests, take ages though...

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,7 @@ mod fen;
 
 fn main() {
     let board = build_board("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -");
-    //uci::main_loop();
-    time_move_search(&build_board(fen::STARTING_FEN), 7);
+    uci::main_loop();
 }
 
 #[allow(dead_code)]
@@ -40,10 +39,17 @@ fn print_moves(board: &Board) {
 
 #[cfg(test)]
 mod move_number_tests {
-    use crate::{search::perft, fen::build_board};
+    use crate::{search::perft, fen::{build_board, self}};
 
+    #[test]
     fn test_position_3() {
         let board = build_board("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -");
-        assert_eq!(1235554, perft(&board, 5));
+        assert_eq!(11030083, perft(&board, 6));
+    }
+
+    #[test]
+    fn test_starting_pos() {
+        let board = build_board(fen::STARTING_FEN);
+        assert_eq!(119060324, perft(&board, 6));
     }
 }
