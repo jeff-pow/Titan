28ac45b56a8b316bf1cf26629c47131c0867e781
Jeff Powell <jrpowel9@ncsu.edu>
2023-10-19 11:30:24 -0400

    .

--- src/eval/nnue.rs
+++ src/eval/nnue.rs
@@ -22,6 +22,10 @@ impl Accumulator {
     fn get(&self, color: Color) -> [i16; LAYER_1_SIZE] {
         self.v[color.idx()]
     }
+
+    fn new(&mut self, bias: &[i16; LAYER_1_SIZE], color_to_update: Color) {
+        self.v[color_to_update.idx()] = *bias;
+    }
 }
 
 #[repr(C)]
@@ -36,6 +40,39 @@ pub struct NetworkState {
     inputs: [[i16; INPUT_SIZE]; 2],
     king_squares: [Square; 2],
     accumulator: Accumulator,
+    net: Network,
+}
+
+impl NetworkState {
+    pub fn new(board: &Board) -> Box<Self> {
+        let mut net: Box<Self> = unsafe {
+            let layout = std::alloc::Layout::new::<Self>();
+            let ptr = std::alloc::alloc_zeroed(layout);
+            if ptr.is_null() {
+                std::alloc::handle_alloc_error(layout);
+            }
+            Box::from_raw(ptr.cast())
+        };
+
+        net
+    }
+
+    fn refresh_accumulators(&mut self, board: &Board) {
+        let white_king = board.king_square(Color::White);
+        let black_king = board.king_square(Color::Black);
+
+        self.accumulator.new(&self.net.feature_bias, Color::White);
+        self.accumulator.new(&self.net.feature_bias, Color::Black);
+
+        for c in Color::iter() {
+            for p in PieceName::iter() {
+                let bb = board.bitboard(c, p);
+                for sq in bb {
+                    // self.update features
+                }
+            }
+        }
+    }
 }
 
 const COLOR_OFFSET: usize = 64 * 6;
