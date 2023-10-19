b3666667911e53c9211d8462acf8fa7c6dfb9b39
Jeff Powell <jrpowel9@ncsu.edu>
2023-10-18 22:54:39 -0400

    .

--- src/search/history.rs
+++ src/search/history.rs
@@ -9,12 +9,20 @@ pub struct MoveHistory {
 }
 
 impl MoveHistory {
-    pub fn update_history(&mut self, m: Move, bonus: i32, side: Color) {
+    fn update_search_history(&mut self, m: Move, bonus: i32, side: Color) {
         let i = &mut self.search_history[side as usize][m.origin_square().idx()][m.dest_square().idx()];
         *i += bonus - *i * bonus.abs() / MAX_HIST_VAL;
     }
 
+    pub fn update_history(&mut self, m: Move, bonus: i32, side: Color) {
+        self.update_search_history(m, bonus, side);
+    }
+
     pub fn get_history(&self, m: Move, side: Color) -> i32 {
+        self.get_search_history(m, side)
+    }
+
+    fn get_search_history(&self, m: Move, side: Color) -> i32 {
         self.search_history[side as usize][m.origin_square().idx()][m.dest_square().idx()]
     }
 }
