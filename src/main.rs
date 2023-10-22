92723c2098f86ecaf5e25231353a934b59d8ca78
Jeff Powell <jrpowel9@ncsu.edu>
2023-10-22 14:31:51 -0400

    .

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,11 @@ pub mod search;
 pub mod types;
 
 use engine::uci::main_loop;
-use crate::board::fen::{build_board, STARTING_FEN};
-use crate::eval::nnue::NET;
-use crate::types::pieces::Color;
 
 fn main() {
     // let mut b = build_board(STARTING_FEN);
     // b.refresh_accumulators();
     // assert_eq!(b.accumulator.get(Color::White), b.accumulator.get(Color::Black));
-    println!("{:?}", &NET);
+    // println!("{:?}", &NET);
     main_loop();
 }
