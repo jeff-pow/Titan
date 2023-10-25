3197f9158d32706d2df6f35a1162ce5e05fa2ca3
Jeff Powell <jeff@drpowell.org>
2023-10-25 18:55:48 -0400

    .

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,17 @@ pub mod types;
 
 use crate::{
     board::fen::{build_board, STARTING_FEN},
+    engine::uci::main_loop,
     eval::nnue::NET,
 };
 
 fn main() {
     let mut b = build_board(STARTING_FEN);
-    b.refresh_accumulators();
+    // b.refresh_accumulators();
     b.accumulator.assert_valid(&b.array_board);
     // should be 22 :(
     println!("{}", b.evaluate());
     println!("{}", NET.feature_weights.iter().filter(|x| **x == 0).count());
     println!("{}", NET.feature_weights.len());
-    // main_loop();
+    main_loop();
 }
