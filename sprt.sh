84f741571b55f150e73cd3063be831a30241dff7
Jeff Powell <jeff@drpowell.org>
2023-11-14 18:13:24 -0500

    .

--- sprt.sh
+++ sprt.sh
@@ -9,7 +9,7 @@ nohup cutechess-cli \
 -engine name=main cmd=./main \
 -games 2 -rounds 50000 \
 -pgnout "pgnout.txt" \
--sprt elo0=0 elo1=5.0 alpha=0.05 beta=0.05 \
+-sprt elo0=0 elo1=3.0 alpha=0.05 beta=0.05 \
 -each proto=uci tc=8+0.08 \
 -openings order=random file="book.pgn" format=pgn \
 -concurrency 6 \
