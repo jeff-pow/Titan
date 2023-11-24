6e0744681d6a64491916c1e30d4cc11f796bdc7d
Jeff Powell <jeff@drpowell.org>
2023-11-23 20:21:55 -0500

    simd accumulator updates ¯\_(ツ)_/¯

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ mod types;
 
 use engine::uci::main_loop;
 
+use crate::board::fen::{build_board, STARTING_FEN};
+
 fn main() {
+    // dbg!(build_board(STARTING_FEN).evaluate());
     main_loop();
 }
