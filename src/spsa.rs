5a52ebd8781a969aa79390b425da82249875498e
Jeff Powell <jeff@drpowell.org>
2023-12-19 12:06:00 -0500

    windows are now automatically 1 or 0.01 to double the current value

--- src/spsa.rs
+++ src/spsa.rs
@@ -25,7 +25,7 @@ pub fn print_ob_tunable_params() {
     for e in &ENTRIES {
         println!(
             "{}, int, {}, {}, {}, {}, 0.002",
-            e.0,
+            e.1.name,
             e.1.val(),
             e.1.range.clone().min().unwrap(),
             e.1.range.clone().max().unwrap(),
@@ -43,11 +43,9 @@ pub fn parse_param(a: &[&str]) {
 }
 
 pub(crate) struct TunableParam {
-    #[allow(dead_code)]
     name: &'static str,
     value: AtomicI32,
     range: RangeInclusive<i32>,
-    #[allow(dead_code)]
     step: f32,
     callback: &'static (dyn Fn() + Sync),
 }
@@ -89,89 +87,53 @@ static ENTRIES: Map<&'static str, &'static TunableParam> = phf_map! {
     "SEE_DEPTH"=> &SEE_DEPTH,
 };
 
-// lazy_static! {
-//     static ref MAP: HashMap<&'static str, &'static TunableParam> = {
-//         let mut map = HashMap::new();
-//         map.insert("LMR_MIN_MOVES", &LMR_MIN_MOVES);
-//         map.insert("LMR_BASE", &LMR_BASE);
-//         map.insert("LMR_DIVISOR", &LMR_DIVISOR);
-//
-//         map.insert("INIT_ASP", &INIT_ASP);
-//         map.insert("ASP_DIVISOR", &ASP_DIVISOR);
-//         map.insert("ASP_MIN_DEPTH", &ASP_MIN_DEPTH);
-//         map.insert("DELTA_EXPANSION", &DELTA_EXPANSION);
-//
-//         map.insert("RFP_BETA_FACTOR", &RFP_BETA_FACTOR);
-//         map.insert("RFP_IMPROVING_FACTOR", &RFP_IMPROVING_FACTOR);
-//         map.insert("RFP_DEPTH", &RFP_DEPTH);
-//
-//         map.insert("NMP_DEPTH", &NMP_DEPTH);
-//         map.insert("NMP_BASE_R", &NMP_BASE_R);
-//         map.insert("NMP_DEPTH_DIVISOR", &NMP_DEPTH_DIVISOR);
-//         map.insert("NMP_EVAL_DIVISOR", &NMP_EVAL_DIVISOR);
-//         map.insert("NMP_EVAL_MIN", &NMP_EVAL_MIN);
-//
-//         map.insert("LMP_DEPTH", &LMP_DEPTH);
-//         map.insert("LMP_IMP_BASE", &LMP_IMP_BASE);
-//         map.insert("LMP_IMP_FACTOR", &LMP_IMP_FACTOR);
-//         map.insert("LMP_NOT_IMP_BASE", &LMP_NOT_IMP_BASE);
-//         map.insert("LMP_NOT_IMP_FACTOR", &LMP_NOT_IMP_FACTOR);
-//
-//         map.insert("QUIET_SEE", &QUIET_SEE);
-//         map.insert("CAPT_SEE", &CAPT_SEE);
-//         map.insert("SEE_DEPTH", &SEE_DEPTH);
-//
-//         map
-//     };
-// }
+tunable_param!(LMR_MIN_MOVES, 2);
+tunable_param!(LMR_BASE, 98, &lmr_reductions);
+tunable_param!(LMR_DIVISOR, 198, &lmr_reductions);
 
-tunable_param!(LMR_MIN_MOVES, 2, 1, 3);
-tunable_param!(LMR_BASE, 100, 50, 150, &lmr_reductions);
-tunable_param!(LMR_DIVISOR, 200, 100, 300, &lmr_reductions);
+tunable_param!(INIT_ASP, 11);
+tunable_param!(ASP_DIVISOR, 9901);
+tunable_param!(ASP_MIN_DEPTH, 4);
+tunable_param!(DELTA_EXPANSION, 1);
 
-tunable_param!(INIT_ASP, 10, 5, 35);
-tunable_param!(ASP_DIVISOR, 10000, 5000, 25000);
-tunable_param!(ASP_MIN_DEPTH, 4, 1, 9);
-tunable_param!(DELTA_EXPANSION, 1, 1, 6);
+tunable_param!(RFP_BETA_FACTOR, 70);
+tunable_param!(RFP_IMPROVING_FACTOR, 35);
+tunable_param!(RFP_DEPTH, 9);
 
-tunable_param!(RFP_BETA_FACTOR, 70, 50, 90);
-tunable_param!(RFP_IMPROVING_FACTOR, 35, 35, 150);
-tunable_param!(RFP_DEPTH, 9, 7, 10);
+tunable_param!(NMP_DEPTH, 3);
+tunable_param!(NMP_BASE_R, 3);
+tunable_param!(NMP_DEPTH_DIVISOR, 3);
+tunable_param!(NMP_EVAL_DIVISOR, 201);
+tunable_param!(NMP_EVAL_MIN, 3);
 
-tunable_param!(NMP_DEPTH, 3, 3, 5);
-tunable_param!(NMP_BASE_R, 3, 1, 6);
-tunable_param!(NMP_DEPTH_DIVISOR, 3, 1, 4);
-tunable_param!(NMP_EVAL_DIVISOR, 200, 100, 300);
-tunable_param!(NMP_EVAL_MIN, 3, 1, 6);
+tunable_param!(LMP_DEPTH, 6);
+tunable_param!(LMP_NOT_IMP_BASE, 97);
+tunable_param!(LMP_NOT_IMP_FACTOR, 49);
+tunable_param!(LMP_IMP_BASE, 252);
+tunable_param!(LMP_IMP_FACTOR, 99);
 
-tunable_param!(LMP_DEPTH, 6, 4, 8);
-tunable_param!(LMP_NOT_IMP_BASE, 100, 50, 300);
-tunable_param!(LMP_NOT_IMP_FACTOR, 50, 30, 150);
-tunable_param!(LMP_IMP_BASE, 250, 100, 300);
-tunable_param!(LMP_IMP_FACTOR, 100, 30, 150);
-
-tunable_param!(QUIET_SEE, 50, 40, 80);
-tunable_param!(CAPT_SEE, 90, 70, 110);
-tunable_param!(SEE_DEPTH, 7, 5, 9);
+tunable_param!(QUIET_SEE, 48);
+tunable_param!(CAPT_SEE, 89);
+tunable_param!(SEE_DEPTH, 7);
 
 #[macro_export]
 macro_rules! tunable_param {
-    ($name:ident, $value:expr, $min:expr, $max:expr, $callback:expr) => {
+    ($name:ident, $value:expr, $callback:expr) => {
         pub static $name: TunableParam = TunableParam {
             name: stringify!($name),
             value: AtomicI32::new($value),
-            range: ($min..=$max),
-            step: max!(0.5, (($max - $min) as f32) / 20.),
+            range: (1..=$value * 2),
+            step: max!(0.5, (($value * 2 - 1) as f32) / 20.),
             callback: $callback,
         };
     };
 
-    ($name:ident, $value:expr, $min:expr, $max:expr) => {
+    ($name:ident, $value:expr) => {
         pub static $name: TunableParam = TunableParam {
             name: stringify!($name),
             value: AtomicI32::new($value),
-            range: ($min..=$max),
-            step: max!(0.5, (($max - $min) as f32) / 20.),
+            range: (1..=$value * 2),
+            step: max!(0.5, (($value * 2 - 1) as f32) / 20.),
             callback: &|| (),
         };
     };
