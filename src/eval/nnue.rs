b91deb5880e5c11b189194f23631fccf4ac32048
Jeff Powell <jeff@drpowell.org>
2023-12-23 18:03:16 -0500

    bench 3105455

--- src/eval/nnue.rs
+++ src/eval/nnue.rs
@@ -91,32 +91,7 @@ unsafe fn flatten(acc: &Block, weights: &Block) -> i32 {
             let weights = _mm256_loadu_epi16(&weights[i * CHUNK_SIZE]);
             sum = _mm256_dpwssd_epi32(sum, crelu_result, weights);
         }
-        // _mm256_reduce_add_epi32(sum)
-        // _mm256_dpwssd_epi32
-        let mut xmm0;
-        let mut xmm1;
-
-        // Get the lower and upper half of the register:
-        xmm0 = _mm256_castsi256_si128(sum);
-        xmm1 = _mm256_extracti128_si256(sum, 1);
-
-        // Add the lower and upper half vertically:
-        xmm0 = _mm_add_epi32(xmm0, xmm1);
-
-        // Get the upper half of the result:
-        xmm1 = _mm_unpackhi_epi64(xmm0, xmm0);
-
-        // Add the lower and upper half vertically:
-        xmm0 = _mm_add_epi32(xmm0, xmm1);
-
-        // Shuffle the result so that the lower 32-bits are directly above the second-lower 32-bits:
-        xmm1 = _mm_shuffle_epi32(xmm0, ((2 << 6) | (3 << 4) | (0 << 2) | 1) as i32); // 2, 3, 0, 1
-
-        // Add the lower 32-bits to the second-lower 32-bits vertically:
-        xmm0 = _mm_add_epi32(xmm0, xmm1);
-
-        // Cast the result to the 32-bit integer type and return it:
-        _mm_cvtsi128_si32(xmm0)
+        hadd(sum)
     }
 
     #[cfg(not(feature = "simd"))]
@@ -127,6 +102,34 @@ unsafe fn flatten(acc: &Block, weights: &Block) -> i32 {
         }
         sum
     }
+}
+
+#[cfg(feature = "simd")]
+unsafe fn hadd(sum: __m256i) -> i32 {
+    let mut xmm0;
+    let mut xmm1;
+
+    // Get the lower and upper half of the register:
+    xmm0 = _mm256_castsi256_si128(sum);
+    xmm1 = _mm256_extracti128_si256(sum, 1);
+
+    // Add the lower and upper half vertically:
+    xmm0 = _mm_add_epi32(xmm0, xmm1);
+
+    // Get the upper half of the result:
+    xmm1 = _mm_unpackhi_epi64(xmm0, xmm0);
+
+    // Add the lower and upper half vertically:
+    xmm0 = _mm_add_epi32(xmm0, xmm1);
+
+    // Shuffle the result so that the lower 32-bits are directly above the second-lower 32-bits:
+    xmm1 = _mm_shuffle_epi32::<0b00000001>(xmm0); // 2, 3, 0, 1
+
+    // Add the lower 32-bits to the second-lower 32-bits vertically:
+    xmm0 = _mm_add_epi32(xmm0, xmm1);
+
+    // Cast the result to the 32-bit integer type and return it:
+    _mm_cvtsi128_si32(xmm0)
 }
 
 #[cfg(test)]
