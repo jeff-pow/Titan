8919c39357fd138dc9496f9625fa8b0bba46ac68
Jeff Powell <jeff@drpowell.org>
2024-01-01 15:56:29 -0500

    .

--- t.sh
+++ t.sh
@@ -7,11 +7,10 @@ rm nohup.out
 cutechess-cli \
 -engine name=dev cmd=./Titan \
 -engine name=main cmd=./Titan \
--games 2 -rounds 50000 \
+-games 1 -rounds 1 \
 -pgnout "pgnout.txt" \
 -sprt elo0=0.0 elo1=3.0 alpha=0.05 beta=0.05 \
 -each proto=uci tc=8+0.08 stderr=stderr.txt \
-
 -concurrency 1 \
 -ratinginterval 10 \
-# -debug \
+-debug \
