1b65b98524818c859b6457d36fe3d813c4971b5c
Jeff Powell <jeff@drpowell.org>
2024-01-11 20:57:32 -0500

    dont crash when parsing random fens... bench: 4244648

--- src/engine/uci.rs
+++ src/engine/uci.rs
@@ -139,7 +139,7 @@ fn position_command(input: &[&str], board: &mut Board, hash_history: &mut Vec<u6
 fn parse_moves(moves: &[&str], board: &mut Board, skip: usize, hash_history: &mut Vec<u64>) {
     for str in moves.iter().skip(skip) {
         let m = from_san(str, board);
-        let _ = board.make_move::<true>(m);
+        let _ = board.make_move::<false>(m);
         hash_history.push(board.zobrist_hash);
     }
 }
