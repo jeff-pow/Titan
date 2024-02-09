00a81371d0854980e9d580e15202b16f2bdf4b49
Jeff Powell <jeff@drpowell.org>
2024-02-09 09:25:20 -0500

    new avx512 compilation method bench: 6352492

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(clippy::module_inception)]
 #![allow(long_running_const_eval)]
-#![cfg_attr(feature = "avx512", feature(stdsimd))]
+#![cfg_attr(feature = "avx512", feature(stdarch_x86_avx512))]
 #[cfg(all(feature = "avx2", feature = "avx512"))]
 compile_error!("Cannot enable both avx2 and avx512 simultaneously.");
 
