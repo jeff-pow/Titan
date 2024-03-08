a4eb55421a296f6c0b1217d30ecec576e4fdf881
Jeff Powell <jeff@drpowell.org>
2024-03-07 19:21:38 -0500

    . bench: 5880505

--- src/search/search.rs
+++ src/search/search.rs
@@ -421,7 +421,7 @@ fn negamax<const IS_PV: bool>(
         // Late Move Reductions - Search moves after the first with reduced depth and window as
         // they are much less likely to be the best move than the first move selected by the
         // move picker.
-        if depth > 2 && moves_searched >= 2 && picker.phase >= MovePickerPhase::Killer {
+        if depth > 2 && moves_searched > 1 && picker.phase >= MovePickerPhase::Killer {
             let mut r = td.lmr.base_reduction(depth, moves_searched);
             if cut_node {
                 r += 1 + i32::from(!m.is_tactical(board));
