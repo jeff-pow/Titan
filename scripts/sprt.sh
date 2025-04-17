64568e64f009dd1816fd0379d9a6c72ac5a5755a
Jeff Powell <jeffersonrpowell@gmail.com>
2025-04-17 13:03:04 -0400

    Different LMP params bench 5743954

--- scripts/sprt.sh
+++ scripts/sprt.sh
@@ -4,14 +4,14 @@
 # rm pgnout.txt
 # rm nohup.out
 
-/home/jeff/ob-client/cutechess-ob \
--engine name=dev cmd=./Titan \
--engine name=main cmd=./Titan \
--games 2 -rounds 250 \
+/home/jeff/ob/Client/cutechess-ob \
+-engine name=dev cmd=./dev \
+-engine name=main cmd=./main \
+-games 2 -rounds 250000 \
 -pgnout "pgnout.txt" \
 -sprt elo0=0.0 elo1=3.0 alpha=0.05 beta=0.05 \
 -each proto=uci tc=8+0.08 stderr=stderr.txt \
--openings order=random file="/home/jeff/ob-client/Books/Pohl.epd" format=epd \
+-openings order=random file="/home/jeff/ob/Books/Pohl.epd" format=epd \
 -concurrency 6 \
 -ratinginterval 10 \
 # -debug \
