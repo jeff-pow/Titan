3978fdabba4bb57822089bb55b9900dcbee039ec
Jeff Powell <jeffersonrpowell@gmail.com>
2025-04-28 18:52:48 -0400

    Tune of net01 with a much lower LR
    bench: 5511705

--- build.rs
+++ build.rs
@@ -4,8 +4,8 @@ use std::{
     process::Command,
 };
 
-const REPO_URL: &str = "https://raw.githubusercontent.com/jeff-pow/FileHydrant/main/";
-const NET_NAME: &str = "net01.bin";
+const REPO_URL: &str = "https://raw.githubusercontent.com/jeff-pow/FileHydrant/net01-tune/";
+const NET_NAME: &str = "net02.bin";
 
 fn main() {
     let path = match env::var("EVALFILE") {
